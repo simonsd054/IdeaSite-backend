59821435c6e99a8c6edfa828f3e7a7341241f6c4
Simon Dahal <simonsd054@gmail.com>
2024-06-24 22:50:00 +1000

    TODO: change friends_ids to junction table in user schema

--- database/schemas/user.js
+++ database/schemas/user.js
@@ -29,6 +29,7 @@ const userSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+    // TODO: change friends_ids to junction table
     fiends_ids: [
       {
         type: mongoose.Schema.Types.ObjectId,
